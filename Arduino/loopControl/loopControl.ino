99278a5133000d5d60f76b678623752d0a41684e
aceroni <aceroni25@gmail.com>
2014-09-24 00:14:44 -0300

    controlador
    Se completa loop de control

--- Arduino/loopControl/loopControl.ino
+++ Arduino/loopControl/loopControl.ino
@@ -1,5 +1,5 @@
 #include <Temp.h>
-//#include <PID_v1.h>
+#include <PID_v1.h>
 #include <GrundfosUP2560.h>
 #include <EqOnOff.h>
 #include <DPL250K.h>
@@ -15,8 +15,20 @@ EqOnOff cooler=EqOnOff(pin_cooler);
 EqOnOff heater=EqOnOff(pin_heater);
 DPL250K flowmeter=DPL250K(pin_fl);
 
+double setpoint, input, output;
+
+PID controlador(&input, &output, &setpoint,2,5,1,DIRECT);
+int WindowSize=5000;
+unsigned long windowStartTime;
+
 void setup(){
   Serial.begin(9600);
+  
+  setpoint=25;
+  windowStartTime=millis();
+  controlador.SetOutputLimits(0, WindowSize);
+  controlador.SetMode(MANUAL);
+  
   getPorts();
   pump.changeStatus(3);
   cooler.apagar();
@@ -24,7 +36,59 @@ void setup(){
 }
 
 void loop(){
+  medir();
+  alarma();
+  controlar();
+  enviar();
+  recibir();
 }
 
 void getPorts(){
 }
+
+void medir(){
+  temp1=t1.medir();
+  temp2=t2.medir();
+  flow=flowmeter.medir();
+  pumpStatus=pump.getStatus();
+  coolerStatus=cooler.getStatus();
+  heaterStatus=heater.getStatus();
+}
+
+void alarma(){
+  if(temp1>60 || temp2>60){
+    heater.apagar();
+  }
+}
+
+void controlar(){
+  input=temp1;
+  controlador.Compute();
+  unsigned long now=millis();
+  if( now-windowStartTime>WindowSize)
+  {
+    windowStartTime+=WindowSize;
+  }
+  if(output>now-windowStartTime) heater.encender();
+  else heater.apagar();
+}
+
+void enviar(){
+  Serial.print("temp1:");
+  Serial.println(temp1);
+  Serial.print("temp2:");
+  Serial.println(temp2);
+  Serial.print("flow:");
+  Serial.println(flow);
+  Serial.print("pump:");
+  Serial.println(pumpStatus);
+  Serial.print("cooler:");
+  Serial.println(coolerStatus);
+  Serial.print("heater:");
+  Serial.println(heaterStatus); 
+  Serial.println("--");
+}
+
+void recibir(){
+}
+
